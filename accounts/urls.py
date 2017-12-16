8e092e7f3abcc2054fc05ac523ade58adbd1fe0e
kimeasyn <kimeasyn@gmail.com>
2017-12-16 14:53:13 +0900

    profile login layout 설정

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth.views import login
+from .views import profile
 
 urlpatterns = [
     url(r'^login/$', login, name='login', kwargs={
         'template_name': 'accounts/login.html',
-    })
+    }),
+    url(r'^profile/$', profile, name='profile'),
 ]
